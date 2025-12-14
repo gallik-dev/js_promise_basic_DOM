f439112be0d5086da6842b38914c017c25e277d5
Maksym Halushko <gallikillag@gmail.com>
2025-12-14 15:36:24 +0200

    fix solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,6 +6,8 @@ const promise1 = new Promise((resolve, reject) => {
   logo.addEventListener('click', () => {
     resolve();
   });
+
+  setTimeout(reject, 3000);
 });
 
 promise1.then(() => {
@@ -16,8 +18,30 @@ promise1.then(() => {
   document.body.appendChild(div);
 });
 
+promise1.catch(() => {
+  const div = document.createElement('div');
+
+  div.className = 'message error-message';
+  div.textContent = 'Promise was rejected!';
+  document.body.appendChild(div);
+});
+
 const promise2 = new Promise((resolve, reject) => {
+  const logo = document.querySelector('.logo');
+
+  logo.addEventListener('click', () => {
+    resolve();
+  });
+
   setTimeout(reject, 3000);
+});
+
+promise2.then(() => {
+  const div = document.createElement('div');
+
+  div.className = 'message';
+  div.textContent = 'Promise was resolved!';
+  document.body.appendChild(div);
 });
 
 promise2.catch(() => {
