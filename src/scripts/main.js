0b9fd57068d704803027497832eeb3e0055580d6
Maksym Halushko <gallikillag@gmail.com>
2025-12-14 20:42:56 +0200

    fix solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,50 +4,44 @@ const promise1 = new Promise((resolve, reject) => {
   const logo = document.querySelector('.logo');
 
   logo.addEventListener('click', () => {
-    resolve();
+    resolve('Success');
   });
-
-  setTimeout(reject, 3000);
 });
 
-promise1.then(() => {
-  const div = document.createElement('div');
+promise1
+  .then(() => {
+    const div = document.createElement('div');
 
-  div.className = 'message';
-  div.textContent = 'Promise was resolved!';
-  document.body.appendChild(div);
-});
+    div.className = 'message';
+    div.textContent = 'Promise was resolved!';
+    document.body.appendChild(div);
+  })
+  .catch(() => {
+    const div = document.createElement('div');
 
-promise1.catch(() => {
-  const div = document.createElement('div');
-
-  div.className = 'message error-message';
-  div.textContent = 'Promise was rejected!';
-  document.body.appendChild(div);
-});
+    div.className = 'message error-message';
+    div.textContent = 'Promise was rejected!';
+    document.body.appendChild(div);
+  });
 
 const promise2 = new Promise((resolve, reject) => {
-  const logo = document.querySelector('.logo');
+  setTimeout(() => {
+    reject(new Error());
+  }, 3000);
+});
 
-  logo.addEventListener('click', () => {
-    resolve();
+promise2
+  .then(() => {
+    const div = document.createElement('div');
+
+    div.className = 'message';
+    div.textContent = 'Promise was resolved!';
+    document.body.appendChild(div);
+  })
+  .catch(() => {
+    const div = document.createElement('div');
+
+    div.className = 'message error-message';
+    div.textContent = 'Promise was rejected!';
+    document.body.appendChild(div);
   });
-
-  setTimeout(reject, 3000);
-});
-
-promise2.then(() => {
-  const div = document.createElement('div');
-
-  div.className = 'message';
-  div.textContent = 'Promise was resolved!';
-  document.body.appendChild(div);
-});
-
-promise2.catch(() => {
-  const div = document.createElement('div');
-
-  div.className = 'message error-message';
-  div.textContent = 'Promise was rejected!';
-  document.body.appendChild(div);
-});
